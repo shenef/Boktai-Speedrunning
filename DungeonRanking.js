a36e0ae45b64452803a2902b6d5ec38b12acdf4b
shenef <matusik.rene@gmx.de>
2021-08-18 00:19:04 +0200

    (mostly) finished the ranking calc

--- DungeonRanking.js
+++ DungeonRanking.js
@@ -1,10 +1,3 @@
-var timeMinutes = 0;
-var timeSeconds = 0;
-var time = timeMinutes * 60 + timeSeconds;
-var found = 0;
-var continues = 0;
-var charged = 0;
-
 const dungeons = [
   ["Crumbling Mine", 45],
   ["Gate of the Dead", 50],
@@ -22,14 +15,34 @@ const dungeons = [
   ["Firetop Mountain", 1500],
 ];
 
+function get_selectedDungeon() {
+  let selection = document.getElementById('selected_dungeon');
+  selectedDungeon = selection.options[selection.selectedIndex].value;
+  return selectedDungeon;
+}
+
 function get_dungeonBaseScore() {
-  dungeonIndex = dungeons.findIndex(dungeon => dungeon[0] == "Ruined Cemetery")
+  dungeonIndex = dungeons.findIndex(dungeon => dungeon[0] == selectedDungeon)
   baseScore = dungeons[dungeonIndex][1];
   return baseScore;
 }
 
-function get_dungeonRank(dungeonScore) {
-  var dungeonScore = document.getElementById('dungeonScore').value;
+function get_dungeonScore() {
+  let time = document.getElementById('dungeonTime').value;
+  let found = document.getElementById('dungeonFound').value;
+  let continues = document.getElementById('dungeonContinues').value;
+  let charged = document.getElementById('dungeonCharged').value;
+
+  let score = 1000 + baseScore - time;
+  if (score > 1000) {
+    score = 1000;
+  }
+  dungeonScore = score - (found * 30) - (continues * 100) + (charged * 10);
+  document.getElementById('dungeonScore').value = dungeonScore;
+  return dungeonScore;
+}
+
+function get_dungeonRank() {
   if (dungeonScore < 100) {rank = "C-"}
   else if (dungeonScore < 300) {rank = "C"}
   else if (dungeonScore < 500) {rank = "C+"}
@@ -40,5 +53,6 @@ function get_dungeonRank(dungeonScore) {
   else if (dungeonScore < 950) {rank = "A"}
   else if (dungeonScore < 1000) {rank = "A+"}
   else {rank = "S"}
-  return document.getElementById('dungeonRank').value = rank;
+  document.getElementById('dungeonRank').value = rank;
+  return rank;
 }
